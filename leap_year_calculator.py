0d010f33401c3df2d04afdbfbb88acc61639a392
Kelhamaa <alexanderkelham@yahoo.co.uk>
2024-01-25 11:33:36 +0000

    broken version

--- leap_year_calculator.py
+++ leap_year_calculator.py
@@ -6,14 +6,14 @@ end_year = 2026
 
 for year in range(start_year, end_year):
     
-    if year%4 == 0 and year%400 == 0:
+    if year%4 == 0 or year%400 == 0:
         leap_year = "Yes"
     elif year%4 == 0 and year%100 == 0:
         leap_year = "No"
     elif year%4 == 0:
         leap_year = "Yes"
     else:
-        leap_year = "No"
+        leap_year = "
     
     if year == this_year:
         print(f"Is this year a leap year? {leap_year}")
